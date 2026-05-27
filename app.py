fa4e28adb733a803bd5a2555b64acea3612036ef
Hasyim <hasyimagiel@gmail.com>
2026-05-27 17:26:25 +0700

    fix ai prompt and ui fix

--- app.py
+++ app.py
@@ -1,34 +1,58 @@
 import streamlit as sl
-# from detection import claories_detection, summary, video_detc
-from detection import video_detc
+from detection import claories_detection, summary, video_detc
 sl.title("Uploaad file")
 sl.markdown("---")
 
 
-vi =  sl.file_uploader("Upload Video", type='mp4', accept_multiple_files=False)
-if sl.button("Upload Video (Only one)"):
-    placeholder = sl.empty()
-    with sl.spinner("Wait in progress....", show_time=True):
-        hasil = video_detc(vi, placeholder)
-        sl.success("Berhasil deteksi")
-        sl.text(hasil)
+gambar = sl.file_uploader(
+    label="Upload Gambar makanan",
+    type=['png', 'jpg'],
+    accept_multiple_files=True
+)
+
+vi =  sl.file_uploader(
+    "Upload Video (only one video)", 
+    type='mp4', 
+    accept_multiple_files=False)
+# if sl.button("Upload Video (Only one)"):
+#     placeholder = sl.empty()
+#     with sl.spinner("Wait in progress....", show_time=True):
+#         hasil = video_detc(vi, placeholder)
+#         sl.success("Berhasil deteksi")
+#         sl.text(hasil)
         
-
-# gambar = sl.file_uploader(
-#     label="Upload Gambar makanan",
-#     type=['png', 'jpg'],
-#     accept_multiple_files=True
-# )
-
-# # if sl.button("coba"):
-# #      print(gambar[0].name)
+if sl.button("Detect"):
+    if gambar is not None:
+        with sl.spinner("Picture detetction progress...", show_time=True):
+            pic, label_pic = claories_detection(gambar)
+            col = sl.columns(len(pic), gap="small")
+            for i in range(len(pic)):
+                with col[i]:
+                    sl.image(pic[i], channels='BGR')
+            sl.success("success detecting picture")
+    else:
+        label_pic = " "
+    
+    if vi is not None:
+        placeholder = sl.empty()
+        with sl.spinner("Video Detetection in progress....", show_time=True):
+            label_vid = video_detc(vi, placeholder)
+            sl.success("Success detecting video")
+    else:
+        label_vid = " "
+    
+    food = label_pic + label_vid
+    with sl.spinner("Summarize results...", show_time=True):
+        summ = summary(food)
+    sl.markdown(summ)
+    
 
 # if sl.button("Detect"):
-#         pic, label= claories_detection(gambar)
-#         summ = summary(label)
-#         col = sl.columns(len(pic), gap="small")
-#         for i in range(len(pic)):
-#             with col[i]:
-#                 sl.image(pic[i], channels='BGR')
-#         sl.text("Summary")
-#         sl.text(summ)
+#     pic, label= claories_detection(gambar)
+#     summ = summary(label)
+#     col = sl.columns(len(pic), gap="small")
+#     for i in range(len(pic)):
+#         with col[i]:
+#             sl.image(pic[i], channels='BGR')
+#     sl.text("Summary")
+#     sl.text(summ)
--- detection.py
+++ detection.py
@@ -11,6 +11,40 @@ model = YOLO("/home/hasyim/projects-ai-engineer/Capston4/best_calories.pt")
 client = OpenAI()
 
 
+    
+def claories_detection(img):
+    food = []
+    food_det = []
+    for imgs in img:
+        #Read image and make temporary file for open cv input
+        temp_pic = tempfile.NamedTemporaryFile(delete=False)
+        temp_pic.write(imgs.read())
+        picture_food=cv2.imread(temp_pic.name)
+        result = model.predict([picture_food])
+        
+        #create the bounding box and label image
+        boxes = result[0].boxes.xyxy.cpu().numpy()
+        confidence = result[0].boxes.conf.cpu().numpy()
+        labels = result[0].boxes.cls.cpu().numpy().astype(int)
+        
+        #box and label maker
+        for box_detect, conf_detetction, label_detection in zip(boxes, confidence, labels):
+            if conf_detetction >= 0.2:
+                x1, y1, x2, y2 = map(int, box_detect)
+                name_label = model.names[label_detection]
+                food.append(name_label)
+                cv2.rectangle(picture_food, (x1,y1), (x2,y2), (0, 255, 0), 3) #make bounding box
+                cv2.putText(picture_food, 
+                            f'{name_label} {conf_detetction:.2f}',
+                            (x1, y1 - 3),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.9,
+                            (255, 0, 0),
+                            2)
+        food_det.append(picture_food)
+    return food_det, ", ".join(food)
+
+
 def video_detc(vid, pengganti):
     temp_vid = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
     temp_vid.write(vid.read())
@@ -41,46 +75,19 @@ def video_detc(vid, pengganti):
     
     video.release()
     os.unlink(temp_vid.name)
-    print("Labelnya adalah: ", foods)
     return foods
-    
-# def claories_detection(img):
-#     food = []
-#     food_det = []
-#     for imgs in img:
-#         #Read image and make temporary file for open cv input
-#         temp_pic = tempfile.NamedTemporaryFile(delete=False)
-#         temp_pic.write(imgs.read())
-#         picture_food=cv2.imread(temp_pic.name)
-#         result = model.predict([picture_food])
-        
-#         #create the bounding box and label image
-#         boxes = result[0].boxes.xyxy.cpu().numpy()
-#         confidence = result[0].boxes.conf.cpu().numpy()
-#         labels = result[0].boxes.cls.cpu().numpy().astype(int)
-        
-#         #box and label maker
-#         for box_detect, conf_detetction, label_detection in zip(boxes, confidence, labels):
-#             if conf_detetction >= 0.2:
-#                 x1, y1, x2, y2 = map(int, box_detect)
-#                 name_label = model.names[label_detection]
-#                 food.append(name_label)
-#                 cv2.rectangle(picture_food, (x1,y1), (x2,y2), (0, 255, 0), 3) #make bounding box
-#                 cv2.putText(picture_food, 
-#                             f'{name_label} {conf_detetction:.2f}',
-#                             (x1, y1 - 3),
-#                             cv2.FONT_HERSHEY_SIMPLEX,
-#                             0.9,
-#                             (255, 0, 0),
-#                             2)
-#         food_det.append(picture_food)
-#     return food_det, ", ".join(food)
 
-# def summary(label):
-#     response = client.responses.create(
-#         model="gpt-4o-mini",
-#         instructions="Kamu adalah ahli gizi yang membantu user dalam hal pemenuhan kalori dari makanan-makanan user, Taruh jumlah kalori paling atas dari gabungan  makanan user ,beri ringkasan singkat maksimal 3 paragraf",
-#         input=f"Berikut adalah makanan yang saya makan {label}",
-#         max_output_tokens=150
-#     )
-#     return response.output_text
+
+def summary(makanan):
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        instructions="""
+        Kamu adalah ahli gizi yang membantu user dalam hal pemenuhan kalori dari makanan-makanan user.
+        Taruh jumlah kalori paling atas dari gabungan makanan user. 
+        beri ringkasan singkat maksimal 3 paragraf.
+        Ringkasan tidak perlu satu maknana 1 paragraf bisa digabungkan.
+        Gunakan emoji agar terlihat kebih friendly
+        """,
+        input=f"Berikut adalah makanan yang saya makan {makanan}",
+    )
+    return response.output_text
