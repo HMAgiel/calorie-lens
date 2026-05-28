deccb5c0a66ad9af1b437147210d293935875b61
Hasyim <hasyimagiel@gmail.com>
2026-05-28 17:21:00 +0700

    chnage training parameter

--- training.py
+++ training.py
@@ -20,24 +20,35 @@ logging.info("=========================================")
 logging.info("Memulai proses training YOLO...")
 start_time = time.time()
 
+from datetime import timedelta
+# 2. Catat Waktu Mulai
+logging.info("=========================================")
+logging.info("Memulai proses training YOLO...")
+start_time = time.time()
+
 try:
     # 3. Proses Training
     results = model.train(
         data='/kaggle/input/datasets/hasyimagiel/data-kalori', 
-        epochs=100,
+        epochs=300,
         device=[0,1],            
-        batch=22,                
+        batch=22,
+        lr0=0.001,
+        lrf=0.0001,
+        warmup_epochs=10,
         imgsz=640,
         hsv_h=0.4,               
         hsv_s=0.6,
         hsv_v=0.5,
         bgr=0.0,                 
-        mosaic=0.5,
-        erasing=0.4,
+        mosaic=0.7,
+        erasing=0.6,
         auto_augment='randaugment',
         exist_ok=True,
-        patience=5,              
-        save_period=5,           
+        patience=50,
+        weight_decay=0.001,
+        dropout=0.1,
+        save_period=50,           
         val=True,                
         verbose=True,            
         flipud=0.0,              
